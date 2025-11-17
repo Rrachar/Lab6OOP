926cd8cfeb346756b157f24963a487f275dcfaa1
Rrachar <g00440528@atu.ie>
2025-11-17 09:17:31 +0000

    start

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,20 @@
+package ie.atu;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int i, int i1) {
+        return i - i1;
+    }
+
+    public int mul(int i, int i1) {
+        return i * i1;
+    }
+
+    public int div(int i, int i1) {
+        return i / i1;
+    }
+}
---
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -0,0 +1,41 @@
+package ie.atu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setup()
+    {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd_Success()
+    {
+        assertEquals(4, calc.add(2,2));
+    }
+
+    @Test
+    public void testSub_Success()
+    {
+        assertEquals(1, calc.sub(3,2));
+    }
+
+    @Test
+    public void testMul_Success()
+    {
+        assertEquals(10, calc.mul(5,2));
+    }
+
+    @Test
+    public void testDiv_Success()
+    {
+        assertEquals(5, calc.div(10,2));
+    }
+
+}
